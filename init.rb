cc1b377ed11ec9bdc0aac21437ac94ce3f38bbde
Steve Schwartz <steve@alfajango.com>
2013-11-05 01:12:12 -0500

    Added Railtie and init.rb support for older versions of Rails, including 2.x and 3.0.

---
+++ init.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+require 'cocoon'
+require 'cocoon/view_helpers'
+ActionView::Base.send :include, Cocoon::ViewHelpers
--- lib/cocoon.rb
+++ lib/cocoon.rb
@@ -1,18 +1,33 @@
 require 'cocoon/view_helpers'
 
 module Cocoon
-  class Engine < ::Rails::Engine
+  if defined? ::Rails::Engine
+    class Engine < ::Rails::Engine
 
-    config.before_initialize do
-      if config.action_view.javascript_expansions
-        config.action_view.javascript_expansions[:cocoon] = %w(cocoon)
+      config.before_initialize do
+        if config.action_view.javascript_expansions
+          config.action_view.javascript_expansions[:cocoon] = %w(cocoon)
+        end
+      end
+
+      # configure our plugin on boot
+      initializer "cocoon.initialize" do |app|
+        ActionView::Base.send :include, Cocoon::ViewHelpers
+      end
+
+    end
+  elsif defined? ::Rails::Railtie
+    class Railtie < ::Rails::Railtie
+      config.before_configuration do
+        if config.action_view.javascript_expansions
+          config.action_view.javascript_expansions[:cocoon] = %w(cocoon)
+        end
+      end
+
+      # configure our plugin on boot
+      initializer "cocoon.initialize" do |app|
+        ActionView::Base.send :include, Cocoon::ViewHelpers
       end
     end
-
-    # configure our plugin on boot
-    initializer "cocoon.initialize" do |app|
-      ActionView::Base.send :include, Cocoon::ViewHelpers
-    end
-
   end
 end
